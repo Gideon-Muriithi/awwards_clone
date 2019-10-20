c8d0539160dc5f058c20bcb2dec677f8116f707f
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-20 21:50:51 +0300

    Add models

--- awwards/admin.py
+++ awwards/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Comment, Project, Rate
 
 admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Comment)
+admin.site.register(Rate) 
+
--- awwards/models.py
+++ awwards/models.py
@@ -1,21 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    bio=models.TextField(max_length=100)
+    bio = models.TextField(max_length=100)
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    # def save(self):
-    #     super().save()
-    #     img = Image.open(self.profile_pic.path)
+class Project(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='project_images/')
+    design = models.IntegerField(default=0)
+    usability = models.IntegerField(default=0)
+    content = models.IntegerField(default=0)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    description = models.TextField(max_length=250)
+    link = models.URLField(max_length=60)
+    date = models.DateField(auto_now=True)
+    screen1 = models.ImageField(upload_to='screenshots/',blank=True)
+    screen2 = models.ImageField(upload_to='screenshots/',blank=True)
 
-    #     if img.height > 300 or img.width > 300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.profile_pic.path)
-       
+    class Meta:
+        ordering=['-title']
+
+    def __str__(self):
+        return self.title
+
+class Rate(models.Model):
+    design = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    usability = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    content = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    project = models.IntegerField(default=0)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    comment = models.TextField(max_length=200)
+    pro_id = models.IntegerField(default=0)
