be8dd8f898ffbe8e3935a3f7f3adb135ca1272bd
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-20 13:41:35 +0300

    Create register view

--- awwards/views.py
+++ awwards/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .forms import UserRegisterForm
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            # return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form':form})
 def home(request):
     return render(request, 'home.html')
